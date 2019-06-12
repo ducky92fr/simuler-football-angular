9b444330c923d4aa74fff3078dbf8411a35680da
Duc NGUYEN <nguyentduc92@gmail.com>
2019-06-12 17:20:30 +0200

    move to page playoff

--- src/app/classement/scoreBoard.component.ts
+++ src/app/classement/scoreBoard.component.ts
@@ -1,17 +1,39 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 import axios from "axios";
+import { JsonPipe } from "@angular/common";
 
 @Component({
   selector: "app-score",
   templateUrl: "./scoreBoard.component.html",
   styleUrls: ["./scoreBoard.component.css"]
 })
-export class ScoreComponent {
+export class ScoreComponent implements OnInit {
+  disabledRun = false;
   results = [];
   startMatch() {
     axios.get("http://localhost:3000/api/match/create").then(res => {
-      console.log(res);
+      this.disabledRun = false;
       this.results = [...res.data.result[0].scoreBoard];
+      console.log(res.data.result[0].week);
+      const scoreBoard = {
+        week: res.data.result[0].week,
+        scoreBoard: this.results
+      };
+      localStorage.setItem("scoreBoard", JSON.stringify(scoreBoard));
     });
+  }
+  ngOnInit() {
+    const scoreBoard = localStorage.getItem("scoreBoard");
+    console.log(scoreBoard);
+    if (scoreBoard) {
+      this.results = [...JSON.parse(scoreBoard).scoreBoard];
+      if (JSON.parse(scoreBoard).week !== 0) {
+        this.disabledRun = false;
+      } else {
+        this.disabledRun = true;
+      }
+    } else {
+      this.disabledRun = false;
+    }
   }
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,7 @@
 import { Component } from "@angular/core";
 import { Team } from "./Team";
+import axios from "axios";
+import { error } from "util";
 
 @Component({
   selector: "app-home",
@@ -7,6 +9,7 @@ import { Team } from "./Team";
   styleUrls: ["./home.component.css"]
 })
 export class HomeComponent {
+  message = "";
   teams = [
     new Team("Barcelone"),
     new Team("Chelsea"),
@@ -20,7 +23,22 @@ export class HomeComponent {
   addTeam() {
     this.teams.push(new Team(""));
   }
-  create() {
-    console.log(this.teams);
+  deleteTeam(index) {
+    console.log(index);
+    this.teams.splice(index, 1);
+  }
+  createLeague() {
+    const arrayTeam = this.teams.map(team => team.Name);
+    axios
+      .post("http://localhost:3000/api/league/create", { arrayTeam: arrayTeam })
+      .then(res => {
+        const scoreBoard = {
+          week: res.data.result.week,
+          scoreBoard: [...res.data.result.scoreBoard]
+        };
+        this.message = "League created";
+        localStorage.setItem("scoreBoard", JSON.stringify(scoreBoard));
+      })
+      .catch(err => console.log(err));
   }
 }
